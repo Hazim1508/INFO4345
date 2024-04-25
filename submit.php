e967fc7dcbc65c1fb67ffbddce81f833e2bc7090
Hazim1508 <121441617+Hazim1508@users.noreply.github.com>
2024-04-25 23:15:15 +0800

    submit.php

--- submit.php
+++ submit.php
@@ -1,4 +1,25 @@
 <?php
+$servername = "localhost";
+$username = "Hazim";
+$password = "INFO4345";
+$dbname = "info4345";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+require 'session_validation.php';
+require 'role_check.php';
+
+if ($_SESSION['role'] != 'admin') {
+    echo "You are not authorized to perform this action.";
+    die();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $matricNo = $_POST["matricNo"];
@@ -17,5 +38,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Email: $email<br>";
     echo "Mobile Phone No: $mobilePhoneNo<br>";
     echo "Home Phone No: $homePhoneNo<br>";
+
+    // sql to create table
+    /*$sql = "CREATE TABLE IF NOT EXISTS student_detail
+    (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        Name VARCHAR(255) NOT NULL UNIQUE,
+        MatricNo INT(7) NOT NULL UNIQUE,
+        CurrentAddress VARCHAR(255),
+        HomeAddress VARCHAR(255),
+        Email VARCHAR(255) NOT NULL UNIQUE,
+        MobilePhoneNo INT(12),
+        HomePhoneNo INT(9)
+    )";*/
+
+    $sql = "INSERT INTO student_detail (Name, MatricNo, CurrentAddress, HomeAddress, Email, MobilePhoneNo, HomePhoneNo) VALUES ('$name', '$matricNo', '$currentAddress', '$homeAddress', '$email', '$mobilePhoneNo', '$homePhoneNo')";
+
+if ($conn->query($sql) === TRUE)
+{
+    echo "New record created successfully";
+    echo "<br><button onclick=\"location.href='form_student.html'\">Add new record</button>";
+} else
+{
+    exit( "Error: " . $sql . "<br>" . $conn->error);
+}
+
+$conn->close();
 }
 ?>
